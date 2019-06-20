a0aef517b402c10b5fa0e51193574e67ea203723
Allan Gao <allangao95@gmail.com>
2019-06-20 12:13:30 +1000

    basic skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./Header/Header";
+import Menu from "./Menu/Menu";
+import Content from "./Content/Content";
+import Footer from "./Footer/Footer";
+
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Menu />
+      <div className="App__container">
+        <Switch>
+          <Route path="/resume" exact component={resume} />
+          <Route path="/projects" exact component={projects} />
+          <Route path="/skills/" exact component={skills} />
+          <Route path="/about" exact component={about} />
+        </Switch>
+        <Content />
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <div className="App__header">Allan Gao</div>
+      <div className="App__subheader">Web Developer</div>
+    </div>
+  );
+}
